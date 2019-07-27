68047a3d2e87de92781c1b7dea3668dc67b36fe6
raul <roaslin@gmail.com>
2019-07-27 20:22:20 +0200

    Test sequence included in kata

--- src/test/java/StatsCalculatorShould.java
+++ src/test/java/StatsCalculatorShould.java
@@ -1,9 +1,13 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class StatsCalculatorShould {
 
     @Test
@@ -15,22 +19,33 @@ public class StatsCalculatorShould {
 
 
     @Test
-    public void print_stats_for_a_sequence_that_contains_number_zero() {
+    @Parameters(method = "sequencesOfNumbers")
+    public void print_stats_for_a_sequence_that_contains_number_zero(int[] sequence, String stats) {
 
         StatsCalculator statsCalculator = new StatsCalculator();
-        assertThat(statsCalculator.process(new int[]{0}), is("o) minimum value = 0\n" +
-                "    o) maximum value = 0\n" +
-                "    o) number of elements in the sequence = 1\n" +
-                "    o) average value = 0.0"));
+        assertThat(statsCalculator.process(sequence), is(stats));
     }
 
-    @Test
-    public void print_stats_for_a_sequence_that_contains_two_numbers() {
+    public Object sequencesOfNumbers() {
 
-        StatsCalculator statsCalculator = new StatsCalculator();
-        assertThat(statsCalculator.process(new int[]{0,1}), is("o) minimum value = 0\n" +
-                "    o) maximum value = 1\n" +
-                "    o) number of elements in the sequence = 2\n" +
-                "    o) average value = 0.5"));
+        return new Object[]{
+                new Object[]{new int[]{0}, "o) minimum value = 0\n" +
+                        "    o) maximum value = 0\n" +
+                        "    o) number of elements in the sequence = 1\n" +
+                        "    o) average value = 0.0"},
+                new Object[]{new int[]{0, 1}, "o) minimum value = 0\n" +
+                        "    o) maximum value = 1\n" +
+                        "    o) number of elements in the sequence = 2\n" +
+                        "    o) average value = 0.5"},
+                new Object[]{new int[]{-100, 100, 200}, "o) minimum value = -100\n" +
+                        "    o) maximum value = 200\n" +
+                        "    o) number of elements in the sequence = 3\n" +
+                        "    o) average value = 66.666664"},
+                new Object[]{new int[]{6, 9, 15, -2, 92, 11}, "o) minimum value = -2\n" +
+                        "    o) maximum value = 92\n" +
+                        "    o) number of elements in the sequence = 6\n" +
+                        "    o) average value = 21.833334"},
+
+        };
     }
 }
